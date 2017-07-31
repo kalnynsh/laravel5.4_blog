0062aa3290d2e94c76acfe7102ccd1f7fe936f35
kalnynsh <kda869@yandex.ru>
2017-07-31 12:25:44 +0300

    Starting project

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use App\Task;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tasks', function() {
+    
+    // $tasks = DB::table('tasks')->latest()->get();
+
+    $tasks = Task::all();  // Обращаемся к модели Task
+
+    return view('tasks.index', compact('tasks'));   
+
+});
+
+Route::get('/tasks/{task}', function($id) {
+
+    // dd($id); // helper dump_var()
+    
+    // $task = DB::table('tasks')->find($id);
+    
+    $task = Task::find($id);
+
+    // Task::incomplete();
+   
+    return view('tasks.show', compact('task'));   // 'tasks/show'
+
+});
