f9f36b481b2f3d999254c5f65fbb3b8845235ddd
kalnynsh <kda869@yandex.ru>
2017-08-03 13:41:54 +0300

    Create class TasksController with index() and show($id) methods.

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//use Illuminate\Http\Request;
+
+use App\Task;  // Import Model Task
+
+
+class TasksController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all();
+
+        return view('tasks.index', compact('tasks')); 
+    }
+    
+    public function show($id) 
+    {
+        $task = Task::find($id);
+ 
+        return view('tasks.show', compact('task'));
+        
+    }
+}
