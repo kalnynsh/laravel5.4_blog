2f696eb2307ea4cbe70ab50cabc84fd343da2f0b
kalnynsh <kda869@yandex.ru>
2017-08-04 12:58:26 +0300

    Create footer.

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="text-muted">
+    
+    <div class="container">
+        
+        <p class="float-right">
+            <a href="#">Back to top</a>
+        </p>
+        
+        <p>Blog © Bootstrap powered</p>
+        
+    </div>
+    
+</footer>
