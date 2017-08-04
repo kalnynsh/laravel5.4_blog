b93059c29a74dfdf9d5170d75b88311461afa985
kalnynsh <kda869@yandex.ru>
2017-08-04 14:02:36 +0300

    Code editting.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -24,8 +24,18 @@
         @include ('layouts.nav') 
         
         @include ('layouts.header') 
+        
+        <div class="container">
+            
+           <div class="row">
                 
-        @yield('content')
+                @yield('content')
+
+                @include('layouts.sidebar')
+
+            </div>
+        
+        </div>
                 
         @include('layouts.footer')
         
