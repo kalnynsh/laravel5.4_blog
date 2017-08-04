ddd006ec82e890acc92a2a3619f4504c90c12ed9
kalnynsh <kda869@yandex.ru>
2017-08-04 14:31:40 +0300

    Remove fileinput.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -28,14 +28,6 @@
             
         </div>
         
-        <div class="form-group">
-            
-            <label for="exampleInputFile">File input</label>
-            <input type="file" id="exampleInputFile">
-            <p class="help-block">Example block-level help text here.</p>
-            
-        </div>
-        
         <div class="checkbox">
             
             <label>
