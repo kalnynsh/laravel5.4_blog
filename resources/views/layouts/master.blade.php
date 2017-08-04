496e4a6087a336dfe13c8792ed94097b7f8f74c5
kalnynsh <kda869@yandex.ru>
2017-08-04 13:28:56 +0300

    Recreate master file with include and yield.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,11 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
-        <meta name="author" content="">
+        <meta name="description" content="Blog">
+        <meta name="author" content="Kalnynsh">
         <link rel="icon" href="../../favicon.ico">
 
-        <title>Album for Blog</title>
+        <title>Blog</title>
 
         <!-- Bootstrap core CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" 
@@ -15,7 +15,7 @@
               crossorigin="anonymous">
         
         <!-- Custom styles for this template -->
-        <link href="/css/album.css" rel="stylesheet">
+        <link href="/css/blog.css" rel="stylesheet">
         
         <!-- JS-->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" 
@@ -23,34 +23,13 @@
             crossorigin="anonymous"></script>
     </head>
 
-    <body>
-
-        <div class="collapse bg-inverse" id="navbarHeader">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-8 py-4">
-                        <h4 class="text-white">About</h4>
-                        <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
-                    </div>
-                    <div class="col-sm-4 py-4">
-                        <h4 class="text-white">Contact</h4>
-                        <ul class="list-unstyled">
-                            <li><a href="#" class="text-white">Follow on Twitter</a></li>
-                            <li><a href="#" class="text-white">Like on Facebook</a></li>
-                            <li><a href="#" class="text-white">Email me</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <body>        
 
         @include ('layouts.nav')
         
-        <div class="container">
+                
+        @yield('content')
         
-            @yield('content')
-
-        </div>
         
         @include('layouts.footer')
 
