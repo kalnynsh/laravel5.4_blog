e959a1c9604dc6023e24f1c361123b98c3e6a7a1
kalnynsh <kda869@yandex.ru>
2017-08-04 14:05:03 +0300

    Code editting.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,15 +1,15 @@
-    <footer class="blog-footer">
-        
-        <p>Blog built for 
-            <a href="http://dev.blog-lv.edu/">
-                dev.blog-lv.edu
-            </a> 
-            by 
-            <a href="https://twitter.com/kalnynsh">
-                @kalnynsh
-            </a>.
-        </p>
-        <p>
-            <a href="#">Back to top</a>
-        </p>
-    </footer>
+<footer class="blog-footer">
+
+    <p>Blog built for 
+        <a href="http://dev.blog-lv.edu/">
+            dev.blog-lv.edu
+        </a> 
+        by 
+        <a href="https://twitter.com/kalnynsh">
+            @kalnynsh
+        </a>.
+    </p>
+    <p>
+        <a href="#">Back to top</a>
+    </p>
+</footer>
