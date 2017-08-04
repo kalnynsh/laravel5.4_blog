498bf1dc5820d1235203fa449e0f7657a6a2c98c
kalnynsh <kda869@yandex.ru>
2017-08-04 14:12:28 +0300

    Add create Route::get('/post/create', ..).

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@
 
 Route::get('/', 'PostsController@index');
 
-Route::get('/posts/{post}', 'PostsController@show');
+Route::get('/posts/create', 'PostsController@create');
+
+//Route::get('/posts/{post}', 'PostsController@show');
