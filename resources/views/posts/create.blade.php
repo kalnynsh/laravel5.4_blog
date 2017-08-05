dc48b8b6d00d7ec5c7f304f4a2f4ce8431b9f3e6
kalnynsh <kda869@yandex.ru>
2017-08-05 13:34:29 +0300

    Add check if count(errors).

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -32,19 +32,29 @@
         
         <button type="submit" class="btn btn-primary">Publish</button>
         
+        @if (count($errors))
+        
+            <div class="form-group">
+
+                <div class="alert alert-warning">
+
+                    <ul>
+                        @foreach ($errors->all() as $error)
+
+                        <li>{{ $error }}</li>
+
+                        @endforeach
+                    </ul>
+
+                </div>
+
+            </div>
+        
+        @endif
+        
     </form>
     
-    <div class="alert alert-warning">
-        
-        <ul>
-            @foreach ($errors->all() as $error)
-            
-            <li>{{ $error }}</li>
-            
-            @endforeach
-        </ul>
-        
-    </div>
+
 
 </div>
 @endsection
