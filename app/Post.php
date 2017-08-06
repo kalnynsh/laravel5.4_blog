6c9bbb31d68214c88ed91c08c2eb31e9a5775f4a
kalnynsh <kda869@yandex.ru>
2017-08-06 11:31:09 +0300

    Add method comments with return hasMany(Comment::class).

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,11 @@ namespace App;
 class Post extends Model
 {
     protected $fillable = ['title', 'body'];
+    
+    public function comments() 
+    {
+    
+        return $this->hasMany(Comment::class);  // 'App\Comment'
+        
+    }
 }
