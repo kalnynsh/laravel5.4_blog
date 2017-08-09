4375626a5babb5c1356fb3d1fad25cc22816a486
kalnynsh <kda869@yandex.ru>
2017-08-09 08:35:33 +0300

    Add Route::post('/register', 'RegistrationController@store').

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,7 @@ Route::post('/posts/{post}/comments', 'CommentsController@store');
 
 Route::get('/register', 'RegistrationController@create');
 
+Route::post('/register', 'RegistrationController@store');
+
 Route::get('/login', 'SessionController@create');
 
