4e06061550397662512cef5856485bcb7d4db866
kalnynsh <kda869@yandex.ru>
2017-08-09 08:15:25 +0300

    Add fields to form Name, Email, Password.

--- resources/views/session/create.blade.php
+++ resources/views/session/create.blade.php
@@ -6,6 +6,43 @@
         
         <h1>Register</h1>
         
+        <form name="registration" method="POST" class="">
+            
+            {{ csrf_field() }}
+            
+            <div class="form-group">
+                
+                <label for="name">Name:</label>
+                
+                <input type="text" class="form-control" 
+                       id="name" name="name">
+                
+            </div>
+                
+            <div class="form-group">
+                    
+                <label for="email">Email:</label>
+                    
+                <input type="email" class="form-group"
+                           id="email" name="email">
+                    
+            </div>
+                
+            <div class="form-group">
+                    
+                <label for="password">Password:</label>
+                    
+                <input type="password" class="form-group"
+                           id="password" name="password">
+                    
+            </div>
+                
+                
+            
+            
+            
+        </form>
+        
     </div>
 
 @endsection
