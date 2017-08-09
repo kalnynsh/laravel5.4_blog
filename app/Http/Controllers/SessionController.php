7a9624f08e2cbcef1c2718690c02ed15724f5f70
kalnynsh <kda869@yandex.ru>
2017-08-09 09:39:27 +0300

    Add destroy method with redirect()->home().

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -6,9 +6,18 @@ use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
-    public function create() {
+    
+    public function create()
+    {
         
+    }
+    
+    public function destroy() 
+    {
         
+        auth()->logout();
+        
+        return redirect()->home();
         
     }
 }
