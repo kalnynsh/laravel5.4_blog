c135421a5a3f5c25ef4ddf31e38c2488f9f75789
kalnynsh <kda869@yandex.ru>
2017-08-09 12:45:24 +0300

    Add method store with if (!auth->attempt(..)){back..} else ..home().

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -10,7 +10,28 @@ class SessionController extends Controller
     public function create()
     {
         
-        return view('session.create');
+        return view('sessions.create');
+        
+    }
+    
+    public function store()
+    {
+// Attempt to authenticate the user.
+        
+        if (!auth()->attempt(request(['email', 'password']))) {
+            
+            return back();
+        }
+        
+// If not, redirect back        
+              
+
+// If so, sign them in.
+        
+
+// Derirect to home page.
+        
+        return redirect()->home();
         
     }
     
