3cb665f12dbd42df8f89e31d2a4c7464e09f3219
kalnynsh <kda869@yandex.ru>
2017-08-09 12:17:40 +0300

    Edit session makeup.

--- resources/views/session/create.blade.php
+++ resources/views/session/create.blade.php
@@ -4,58 +4,7 @@
 
     <div class="col-sm-8">
         
-        <h1>Register</h1>
-        
-        <form name="registration" method="POST" action="/register">
-            
-            {{ csrf_field() }}
-            
-            <div class="form-group">
-                
-                <label for="name">Name:</label>
-                
-                <input type="text" class="form-control" 
-                       id="name" name="name" required>
-                
-            </div>
-                
-            <div class="form-group">
-                    
-                <label for="email">Email:</label>
-                    
-                <input type="email" class="form-control"
-                           id="email" name="email" required>
-                    
-            </div>
-                
-            <div class="form-group">
-                    
-                <label for="password">Password:</label>
-                    
-                <input type="password" class="form-control"
-                           id="password" name="password" required>
-                    
-            </div>
-                
-            <div class="form-group">
-                    
-                <label for="password_confirmation">Password Confirmation:</label>
-                    
-                <input type="password" class="form-control"
-                           id="password_confirmation" 
-                           name="password_confirmation" required>
-                    
-            </div>
-                
-            <div class="form-group">                    
-                                    
-                <button type="submit" class="btn btn-primary">Register</button>
-                    
-            </div>
-                                        
-                @include ('layouts.errors')
-  
-        </form>
+        <h1>Sign In</h1>
         
     </div>
 
