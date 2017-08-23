034f42f57ed841086e4fc750d475ab0bf37cef2c
kalnynsh <kda869@yandex.ru>
2017-08-23 14:13:33 +0300

    Move scopeFilter($query, $filters) {if ($month = $filters['month'])..} to Post.php.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -16,21 +16,9 @@ class PostsController extends Controller
     
     public function index() {     
          
-        $posts = Post::latest();
-        
-        if ($month = request('month')) {
-            
-            $posts->whereMonth('created_at', $month);
-            
-        }
-        
-        if ($year = request('year')) {
-            
-            $posts->whereYear('created_at', $year);
-            
-        }
-        
-        $posts = $posts->get();
+        $posts = Post::latest()
+                ->filter(request(['month', 'year']))
+                ->get();       
         
         $archives = Post::selectRaw(
                 "DATE_PART('year', created_at) AS year, "
