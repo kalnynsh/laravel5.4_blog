798f320bec8a76b2bf625d464c1c510e370d7f9d
kalnynsh <kda869@yandex.ru>
2017-08-25 12:33:40 +0300

    Add to ExampleTast use DatabaseTransactions;

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,12 +2,19 @@
 
 namespace Tests\Unit;
 
+use App\Post;
+
 use Tests\TestCase;
+
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+
 
 class ExampleTest extends TestCase
 {
+    use DatabaseTransactions;
+    
     /**
      * A basic test example.
      *
@@ -15,6 +22,24 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        
+// Given. I have two recorde in the database that are posts,
+//        and each one is posted a month apart.
+        
+        $first = factory(Post::class)->create();
+        
+        $second = factory(Post::class)->create([
+            
+            'created_at' => \Carbon\Carbon::now()->subMonth()
+                
+        ]);
+        
+// When I fetch the archives.
+        $posts = Post::archives();
+
+// Then the response should be in the proper format.  
+        
+        $this->assertCount(2, $posts);
+        
     }
 }
