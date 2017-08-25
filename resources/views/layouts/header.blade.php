d14bee92bc59213c0d50c317c03d0783bd6d82a6
kalnynsh <kda869@yandex.ru>
2017-08-25 10:58:58 +0300

    Change header text.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,9 +2,9 @@
 
     <div class="container">
 
-        <h1 class="blog-title">The Bootstrap Blog</h1>
+        <h1 class="blog-title">The Blog</h1>
 
-        <p class="lead blog-description">An example blog template built with Bootstrap.</p>
+        <p class="lead blog-description">The blog builts with Bootstrap.</p>
 
     </div>
 
