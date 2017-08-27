8f034556958c59c133057e531c6c31a07f326d0b
kalnynsh <kda869@yandex.ru>
2017-08-27 11:46:00 +0300

    Use App:singleton('App\Billing\Stripe',..) instead App::bind().

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,18 @@
 |
 */
 
-App::bind('App\Billing\Stripe', function() {
+App::singleton('App\Billing\Stripe', function() {
 
     return new \App\Billing\Stripe(config('services.stripe.secret'));
     
 });
 
 
-//$stripe = App::make('App\Billing\Stripe');
+// $stripe = App::make('App\Billing\Stripe');
+// $stripe = app('App\Billing\Stripe');
 
-$stripe = resolve('App\Billing\Stripe');
-
-dd($stripe);        
+// $stripe = resolve('App\Billing\Stripe');
+// dd($stripe);        
         
 
 Route::get('/', 'PostsController@index')->name('home');
