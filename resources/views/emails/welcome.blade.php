551dd8dcdd011c217e0a620eccee8ab49a3b16ac
kalnynsh <kda869@yandex.ru>
2017-08-29 08:30:48 +0300

    Create makeup.

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,8 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Email</title>
+</head>
+<body>
+    
+    <h1>Welcome to Laracasts!</h1>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+</body>
+</html>
 
