839891330c1240ebee11f8f3460296a1b3bf6ed0
kalnynsh <kda869@yandex.ru>
2017-09-01 11:21:28 +0300

    Add to method up() $table->string('name')->unique();

--- database/migrations/2017_09_01_081317_create_tags_table.php
+++ database/migrations/2017_09_01_081317_create_tags_table.php
@@ -14,8 +14,13 @@ class CreateTagsTable extends Migration
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
+            
             $table->increments('id');
+            
+            $table->string('name')->unique();
+                    
             $table->timestamps();
+            
         });
     }
 
