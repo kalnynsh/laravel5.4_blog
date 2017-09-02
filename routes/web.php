83875c8fe57c54c454a425c73a916604cf307fad
kalnynsh <kda869@yandex.ru>
2017-09-02 11:31:55 +0300

    Add Route::get('/posts/tags/{tag}', 'PostsController@index');

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Route::get('/posts/{post}', 'PostsController@show');
 Route::post('/posts/{post}/comments', 'CommentsController@store');
 
 
+Route::get('/posts/tags/{tag}', 'PostsController@index');
+
+
 Route::get('/register', 'RegistrationController@create');
 
 Route::post('/register', 'RegistrationController@store');
