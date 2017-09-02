5ffe5ca791ae11eb3d769121e56dcebca5a8b93e
kalnynsh <kda869@yandex.ru>
2017-09-02 12:10:38 +0300

    Edit return view('posts.index', compact('posts'));

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -13,7 +13,7 @@ class TagsController extends Controller
         
         $posts = $tag->posts;
         
-        return view('posts.index', compact($posts)); 
+        return view('posts.index', compact('posts')); 
         
     }
 }
